3254eb9ca5102bf105f6ca3819afce44070d8084
wwroblewska <weraw16@gmail.com>
2017-05-26 18:58:11 +0200

    Add files via upload

---
+++ klasy.cpp
@@ -0,0 +1,24 @@
+#include "klasy.h"
+#include "stdafx.h"
+
+Astronaut::Astronaut() {
+	x = 25;
+	y = 0;
+
+
+}
+Astronaut::~Astronaut() {}
+
+Meteorite::Meteorite() {
+}
+Meteorite::~Meteorite() {
+}
+Planet::Planet() {
+}
+Planet::~Planet() {
+}
+Shield::Shield() {
+}
+Shield::~Shield() {}
+Board::~Board() {
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "klasy.h"
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
+
