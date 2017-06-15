ce0af1e92fb561cbb72820623112abe59748c0ae
wwroblewska <weraw16@gmail.com>
2017-06-15 23:49:14 +0200

    Update Offer.cpp

--- Offer.cpp
+++ Offer.cpp
@@ -1,76 +1,77 @@
+#include "stdafx.h"
 #include "Offer.h"
 #include <iostream>
 #include <cstdlib>
 #include <vector>
 
 Offer::Offer(const std::string &title, const std::string &description, const std::string &keywords, double price)
-    : title(title), description(description), keywords(keywords), price(price)
+	: title(title), description(description), keywords(keywords), price(price)
 {
 
 }
 
 const std::string& Offer::getTitle() const {
-    return title;
+	return title;
 }
 
 const std::string& Offer::getDescription() const {
-    return description;
+	return description;
 }
 
 const std::string& Offer::getKeywords() const {
-    return keywords;
+	return keywords;
 }
 
 double Offer::getPrice() const {
-    return price;
+	return price;
 }
 
 void Offer::setDescription(const std::string &d) {
-    description = d;
+	description = d;
 }
 
 void Offer::setKeywords(const std::string &k) {
-    keywords = k;
+	keywords = k;
 }
 
 void Offer::setTitle(const std::string &t) {
-    title = t;
+	title = t;
 }
 
 void Offer::setPrice(double p) {
-    price = p;
+	price = p;
 }
 
 
 std::ostream& operator<<(std::ostream& os, const Offer& offer) {
-    os << offer.title << "\n" << offer.description  << offer.keywords << "\n" << offer.price;
-    return os;
+	os << offer.title << "\n" << offer.description << offer.keywords << "\n" << offer.price;
+	return os;
 }
 
 Offer Offer::createOfferFromText(std::vector<std::string>& lines) {
-    Offer out(" ", " ", " ", 0.0);
+	Offer out(" ", " ", " ", 0.0);
 
 
-    out.setTitle(lines[0]);
-    out.setKeywords(lines[lines.size()-2]);
-    out.setPrice(atof(lines[lines.size()-1].c_str()));
+	out.setTitle(lines[0]);
+	out.setKeywords(lines[lines.size() - 2]);
+	out.setPrice(atof(lines[lines.size() - 1].c_str()));
 
 
-    std::string combinedDescription;
-    for(int i = 1; i < lines.size() - 2; i++) {
-        combinedDescription += lines[i] + "\n";
-    }
+	std::string combinedDescription;
+	for (int i = 1; i < lines.size() - 2; i++) {
+		combinedDescription += lines[i] + "\n";
+	}
 
-    out.setDescription(combinedDescription);
+	out.setDescription(combinedDescription);
 
-    //std::cout << "Stworzono oferte: \n" << out << std::endl;
+	//std::cout << "Stworzono oferte: \n" << out << std::endl;
 
-    return out;
+	return out;
 }
 
 void Offer::print(std::ostream &os) const {
-    std::cout << "\nNazwa oferty:\t" << title << std::endl;
-    std::cout << "Cena:\t" << price << std::endl << std::endl;
-    std::cout << "Opis: " << std::endl << description << std::endl;
-    std::cout << "Slowa kluczowe: " << keywords << std::endl;
+	std::cout << "\nNazwa oferty:\t" << title << std::endl;
+	std::cout << "Cena:\t" << price << std::endl << std::endl;
+	std::cout << "Opis: " << std::endl << description << std::endl;
+	std::cout << "Slowa kluczowe: " << keywords << std::endl;
 }
