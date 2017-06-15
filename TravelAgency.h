66ffc02fc9845b62ebc0853afdc45f072ffb524b
wwroblewska <weraw16@gmail.com>
2017-06-15 23:50:21 +0200

    Update TravelAgency.h

--- TravelAgency.h
+++ TravelAgency.h
@@ -2,20 +2,30 @@
 
 #include <vector>
 #include "Offer.h"
+#include "Client.h"
 
 class TravelAgency {
-    std::vector<Offer> offers;
+	std::vector<Offer> offers;
 
-    std::vector<std::string> readFile(const std::string& filePath);
-    std::vector<Offer> processFileLines(std::vector<std::string>& lines);
-    void readAllOffers();
-    void printAllOffers() const;
+	std::vector<std::string> readFile(const std::string& filePath);
+	std::vector<Offer> processFileLines(std::vector<std::string>& lines);
+	void readAllOffers();
+	void printAllOffers() const;
 
-    void displayOffersByKeywords();
+	void displayOffersByKeywords();
+	void displayOffersInPriceRange() const;
+	void selectOfferDialog();
 
+
+	bool checkIfOfferExsists(const std::string& name);
+	void makeOrder(const std::string& offer, const Client& client);
 
 public:
-    TravelAgency();
-    void menu();
+	TravelAgency();
+	void menu();
+
+	void addReviewDialog();
+
+	void showReviewDialog();
 };
 
